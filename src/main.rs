1ef05eb62e24d4e54eec1abb40ef922b12cc03c7
theketan26 <ketangsolanki129@gmail.com>
2024-07-18 00:52:52 +0530

    added rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+use rocket::get;
+
+
+#[get("/greet")]
+fn hello() -> String {
+    String::from("Hello World!")
+}
+
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/api", routes![hello])
 }
