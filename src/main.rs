d333386e429f99242a02e680b6fa5ba3e44d9b65
theketan26 <ketangsolanki129@gmail.com>
2024-07-21 12:33:09 +0530

    mongo db integrated

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+extern crate dotenv_codegen;
+use dotenv_codegen::dotenv;
+
 #[macro_use] extern crate rocket;
+use mongodb::{bson::doc, options::{ClientOptions, ServerApi, ServerApiVersion}, Client};
 
 use rocket::get;
 
@@ -9,8 +13,24 @@ fn hello() -> String {
 }
 
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build()
+#[tokio::main]
+async fn main() {
+    let mut client_options = ClientOptions::parse(dotenv!("MONGO_DB")).await.unwrap();
+
+    let server_api = ServerApi::builder().version(ServerApiVersion::V1).build();
+    client_options.server_api = Some(server_api);
+    
+    let client = Client::with_options(client_options).unwrap();
+    
+    client
+        .database("admin")
+        .run_command(doc! {"ping": 1})
+        .await
+        .unwrap();
+    println!("Pinged your deployment. You successfully connected to MongoDB!");
+
+    let _ = rocket::build()
         .mount("/api", routes![hello])
+        .launch()
+        .await;
 }
